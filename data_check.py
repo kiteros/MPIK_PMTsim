6aa65431976d2f9ff9b564573bad9505fa531af1
Johannes Bennemann <johannes.bennemann@mpi-hd.mpg.de>
2021-08-06 16:38:33 +0200

    Added particle destiction.

--- data_check.py
+++ data_check.py
@@ -10,13 +10,15 @@ if makePlots:
     plt.scatter(east, north, c="grey")
 
 # shower events
-events = uproot.lazy("data/DAT000001.root:XCDF")
+events = uproot.lazy("data/*.root:XCDF")
 binsAll = np.linspace(-5,170)
 histAll = np.zeros(binsAll.shape[0]-1)
 upperAll = np.zeros(pmtIdG.shape)
 lowerAll = np.zeros(pmtIdG.shape)
+particles = np.zeros(70)
+parTicks = ([2,3,5,6,8,9,11,12,14,15],["$e^+$","$e^-$","$\mu^+$","$\mu^-$","$\pi^+$","$\pi^-$","$K^+$","$K^-$","$p$","$\\bar p$"])
 cdx = 0
-for pmtIds, peTimes, ft, ex, ey in zip(events["HAWCSim.PE.PMTID"], events["HAWCSim.PE.Time"], events["HAWCSim.Evt.firstTime"], events["HAWCSim.Evt.X"], events["HAWCSim.Evt.Y"]):
+for pmtIds, peTimes, ft, ex, ey, parPType in zip(events["HAWCSim.PE.PMTID"], events["HAWCSim.PE.Time"], events["HAWCSim.Evt.firstTime"], events["HAWCSim.Evt.X"], events["HAWCSim.Evt.Y"], events["HAWCSim.PE.parPType"]):
     # plot all
     peTimes = peTimes.to_numpy()-ft
     pmtIds = pmtIds.to_numpy()
@@ -53,22 +55,39 @@ for pmtIds, peTimes, ft, ex, ey in zip(events["HAWCSim.PE.PMTID"], events["HAWCS
         plt.scatter(upper,lower)
         #plt.xscale("log")
         #plt.yscale("log")
+    # get particle types
+    parPType = parPType.to_numpy()
+    pTypes, pCnts = np.unique(parPType, return_counts=True)
+    particles[pTypes] += pCnts
+    if makePlots:
+        plt.figure(3)
+        plt.bar(pTypes, pCnts)
+        plt.xticks(*parTicks)
     # carry over
     histAll += hist
     upperAll += upper
     lowerAll += lower
     cdx += 1
     #if cdx > 5: break
+    #break
 
 print(cdx)
+# fig 7 (times)
 plt.figure(7)
 plt.plot(binsAll[:-1], histAll)
 plt.yscale("log")
+# fig 13 (upper/lower ratio)
 plt.figure(13)
 hist, xedges, yedges = np.histogram2d(upperAll,lowerAll, 100)
-plt.pcolormesh(*np.meshgrid(xedges, yedges), np.log(hist))
+plt.pcolormesh(*np.meshgrid(xedges, yedges), np.log(hist+1))
 plt.colorbar()
-np.save("data/is_upper.npy",upperAll)
-np.save("data/is_lower.npy",lowerAll)
+plt.xscale("log")
+plt.yscale("log")
+#np.save("data/is_upper.npy",upperAll)
+#np.save("data/is_lower.npy",lowerAll)
+# particles
+plt.figure(3)
+plt.bar(np.arange(16), particles[:16]) # ignore resonaces and neutrinos
+plt.xticks(*parTicks)
 
 plt.show()
