4bcf97e2684bb22745a878142c73b697ef391a89
Johannes Bennemann <johannes.bennemann@mpi-hd.mpg.de>
2021-08-10 16:42:53 +0200

    Individual histograms for electrons and muons.

--- data_check.py
+++ data_check.py
@@ -2,6 +2,19 @@ import uproot
 import numpy as np
 from matplotlib import pyplot as plt
 
+TAG_E = 1
+TAG_MU = 2
+TAG_MESON = 4
+TAG_OTHER = 8
+def makeTag(parts):
+    tag = 0
+    for p in parts:
+        if p < 4: tag |= TAG_E
+        elif p >= 5 and p < 7: tag |= TAG_MU
+        elif p >= 7 and p < 13: tag |= TAG_MESON
+        else: tag |= TAG_OTHER
+    return tag
+
 # detector geometry
 pmtIdG, east, north = np.loadtxt("data/swgo_reference_survey.txt", unpack=True)
 makePlots = False
@@ -10,13 +23,14 @@ if makePlots:
     plt.scatter(east, north, c="grey")
 
 # shower events
-path = "data/gammabbww/" # 5, 46 broken?
+path = "data/protonbbww/" # gamma 5, 46 broken?
 events = uproot.lazy(path+"*.root:XCDF")
 binsAll = np.linspace(-5,170)
 histAll = np.zeros(binsAll.shape[0]-1)
 xedges = np.linspace(10**0,10**4,1001)
 yedges = np.linspace(10**0,10**4,1001)
 histULAll = np.zeros((xedges.shape[0]-1,yedges.shape[0]-1))
+taggedPmtEvts = []
 particles = np.zeros(70)
 parTicks = ([2,3,5,6,8,9,11,12,14,15],["$e^+$","$e^-$","$\mu^+$","$\mu^-$","$\pi^+$","$\pi^-$","$K^+$","$K^-$","$p$","$\\bar p$"])
 cdx = 0
@@ -24,6 +38,7 @@ for pmtIds, peTimes, ft, ex, ey, parPType in zip(events["HAWCSim.PE.PMTID"], eve
     # plot all
     peTimes = peTimes.to_numpy()-ft
     pmtIds = pmtIds.to_numpy()
+    parPType = parPType.to_numpy()
     if makePlots:
         plt.figure(2)
         plt.scatter(peTimes, pmtIds, c="grey")
@@ -36,6 +51,14 @@ for pmtIds, peTimes, ft, ex, ey, parPType in zip(events["HAWCSim.PE.PMTID"], eve
         plt.scatter(east[(pmtIdP/2).astype(int)], north[(pmtIdP/2).astype(int)], vmin=cntPL.min(), vmax=cntPL.max(), c=cntPL, cmap=cm)
         plt.scatter(ex,ey,marker="x")
         plt.gca().add_patch(plt.Circle((ex,ey),20*100,fill=False))
+    # tag pmt signals
+    tagsUpper = np.zeros(pmtIdG.shape,dtype=int)
+    tagsLower = np.zeros(pmtIdG.shape,dtype=int)
+    for id in pmtIdP:
+        if id%2 == 0:
+            tagsUpper[int(id/2)] |= makeTag(np.unique(parPType[pmtIds == id]))
+        if id%2 == 1:
+            tagsLower[int(id/2)] |= makeTag(np.unique(parPType[pmtIds == id]))
     # make time histogram
     hist, bins = np.histogram(peTimes, binsAll)
     if makePlots:
@@ -59,7 +82,6 @@ for pmtIds, peTimes, ft, ex, ey, parPType in zip(events["HAWCSim.PE.PMTID"], eve
         #plt.xscale("log")
         #plt.yscale("log")
     # get particle types
-    parPType = parPType.to_numpy()
     pTypes, pCnts = np.unique(parPType, return_counts=True)
     particles[pTypes] += pCnts
     if makePlots:
@@ -69,6 +91,8 @@ for pmtIds, peTimes, ft, ex, ey, parPType in zip(events["HAWCSim.PE.PMTID"], eve
     # carry over
     histAll += hist
     histULAll += histUL
+    sel = np.logical_or(upper > 0,lower > 0)
+    taggedPmtEvts.append((cdx*np.ones(pmtIdG[sel].shape), pmtIdG[sel], upper[sel], lower[sel], tagsUpper[sel], tagsLower[sel]))
     cdx += 1
     #if cdx > 5: break
     #break
@@ -84,12 +108,30 @@ plt.pcolormesh(*np.meshgrid(xedges, yedges), np.log(histULAll))
 plt.colorbar()
 plt.xscale("log")
 plt.yscale("log")
-np.save(path+"histUL.npy",histULAll)
-np.save(path+"xedges.npy",xedges)
-np.save(path+"yedges.npy",yedges)
+#np.save(path+"histUL.npy",histULAll)
+#np.save(path+"xedges.npy",xedges)
+#np.save(path+"yedges.npy",yedges)
 # particles
 plt.figure(3)
 plt.bar(np.arange(16), particles[:16]) # ignore resonaces and neutrinos
 plt.xticks(*parTicks)
+
+# save tagged events
+saveTaggedEvts = False
+if saveTaggedEvts:
+    cols = 6
+    rows = 0
+    for e in taggedPmtEvts:
+        rows += e[0].size
+    data = np.zeros((cols, rows))
+    off = 0
+    for e in taggedPmtEvts:
+        newOff = off + e[0].shape[0]
+        for i in np.arange(cols):
+            data[i,off:newOff] = e[i]
+        off = newOff
+    np.save(path+"taggedPmtEvts.npy",data)
+
+
 
 plt.show()
--- quick_hist.py
+++ quick_hist.py
@@ -1,8 +1,14 @@
 import numpy as np
 from matplotlib import pyplot as plt
 
+#TODO import
+TAG_E = 1
+TAG_MU = 2
+TAG_MESON = 4
+TAG_OTHER = 8
+
 # data
-paths = ["data/protonbbww/", "data/gammabbww/"]
+paths = ["data/protonbbww/"]#, "data/gammabbww/"]
 for path in paths:
     xedges = np.load(path+"xedges.npy")
     yedges = np.load(path+"yedges.npy")
@@ -15,6 +21,30 @@ for path in paths:
     plt.colorbar()
     plt.xscale("log")
     plt.yscale("log")
+
+    # make individual histograms
+    taggedPmtEvts = np.load(path+"taggedPmtEvts.npy")
+    upper = taggedPmtEvts[2]
+    lower = taggedPmtEvts[3]
+    tags = taggedPmtEvts[4].astype(int)
+    tags |= taggedPmtEvts[5].astype(int)
+    # select type
+    eOnly = tags ^ TAG_E == 0
+    muOnly = tags ^ TAG_MU == 0
+    muAny = tags & TAG_MU > 0
+    # histogram
+    histEOnly, xedges, yedges = np.histogram2d(upper[eOnly],lower[eOnly], bins=[xedges,yedges])
+    histMuOnly, xedges, yedges = np.histogram2d(upper[muOnly],lower[muOnly], bins=[xedges,yedges])
+    histMuAny, xedges, yedges = np.histogram2d(upper[muAny],lower[muAny], bins=[xedges,yedges])
+    # plot
+    for title, hist in zip(["Only electrons", "Only muons", "Any muons"], [histEOnly, histMuOnly, histMuAny]):
+        plt.figure()
+        plt.title(title)
+        plt.pcolormesh(*np.meshgrid(xedges, yedges), np.log(hist))
+        plt.colorbar()
+        plt.xscale("log")
+        plt.yscale("log")
+
 
 
 plt.show()
